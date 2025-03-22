cc902372831d8049e90939c64f333f293136e4c4
AHappyFox <mrfox1910@gmail.com>
2025-03-22 11:54:50 +0100

    Added support for configurable Output - either as an XML-file or directly in the Shell

--- Create-MultipleAppLockerRules.ps1
+++ Create-MultipleAppLockerRules.ps1
@@ -1,20 +1,34 @@
 function Create-MultipleAppLockerRules {
     param (
         [Parameter(Mandatory=$True)]
-        [string] $FilePath
+        [string] $FilePath,
+
+        [Parameter(Mandatory=$False)]
+        [string] $Description,
+
+        [ValidateSet ("XML", "Shell")]
+        [Parameter(Mandatory=$True)]
+        [string] $Output
     )
 
     $Files = (Get-ChildItem -Path $FilePath).FullName
+    if (Test-Path -Path "$env:USERPROFILE\Documents\AppLocker\AppLockerRules.xml") {
+        Remove-Item -Path "$env:USERPROFILE\Documents\AppLocker\AppLockerRules.xml" -Force
+    }
 
     foreach ($File in $Files) {
         $IsSigned = (Get-AppLockerFileInformation -Path $File).Publisher
         if (($IsSigned) -eq $null) {
             #Write-Host "$File is not signed"
-            New-AppLockerHashRule -FilePath $File
+            New-AppLockerHashRule -FilePath $File -Description $Description -OutPut $Output
         }
         else {
             #Write-Host "$File is signed with ($IsSigned)"
-            New-AppLockerPublisherRule -FilePath $File
+            New-AppLockerPublisherRule -FilePath $File -Description $Description -OutPut $Output
         }
+    }
+
+    if ($Output -eq "XML") {
+    Write-Host "The XML file can be located here: '$env:USERPROFILE\Documents\AppLocker\AppLockerRules.xml'"
     }
 }
